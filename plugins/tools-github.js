045e04c990983fbb95461ff26c621b987c6275f1
puffino26 <maxjr0030@gmail.com>
2025-04-17 00:58:40 +0200

    Update tools-github.js

--- plugins/tools-github.js
+++ plugins/tools-github.js
@@ -1,12 +1,24 @@
-import fetch from 'node-fetch'
+import fetch from 'node-fetch';
 
-const regex = /(?:https|git)(?:://|@)github.com/:/(.+)/i var handler = async (m, { args, usedPrefix, command }) => {
+const regex = /(?:https|git)(?::\/\/|@)github.com\/(.+)/i;
 
-if (!args[0]) return conn.reply(m.chat, > ⓘ 𝐔𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix + command} https://github.com/GabWT333/Gab333, m, ) if (!regex.test(args[0])) conn.reply(m.chat, > ⚠️ 𝐈𝐧𝐯𝐢𝐚 𝐮𝐧 𝐥𝐢𝐧𝐤 𝐯𝐚𝐥𝐢𝐝𝐨, 𝐮𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix + command} https://github.com/GabWT333/Gab333, m, )
+var handler = async (m, { args, usedPrefix, command }) => {
+  if (!args[0]) 
+    return conn.reply(m.chat, `> ⓘ 𝐔𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix + command} https://github.com/puffino26/Puffinobot.git`, m);
+  
+  if (!regex.test(args[0])) 
+    return conn.reply(m.chat, `> ⚠️ 𝐈𝐧𝐯𝐢𝐚 𝐮𝐧 𝐥𝐢𝐧𝐤 𝐯𝐚𝐥𝐢𝐝𝐨, 𝐮𝐬𝐨 𝐝𝐞𝐥 𝐜𝐨𝐦𝐚𝐧𝐝𝐨:\n> ${usedPrefix + command} https://github.com/puffino26/Puffinobot.git`, m);
 
-let [_, user, repo] = args[0].match(regex) || [] repo = repo.replace(/.git$/, '') let url = https://api.github.com/repos/${user}/${repo}/zipball let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1] conn.reply(m.chat, ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ..., m, ) conn.sendFile(m.chat, url, filename, null, m)
+  let [_, user, repo] = args[0].match(regex) || [];
+  repo = repo.replace(/.git$/, '');
+  let url = `https://api.github.com/repos/${user}/${repo}/zipball`;
+  let filename = (await fetch(url, { method: 'HEAD' })).headers.get('content-disposition').match(/attachment; filename=(.*)/)[1];
+  
+  conn.reply(m.chat, `ⓘ 𝐂𝐚𝐫𝐢𝐜𝐚𝐦𝐞𝐧𝐭𝐨 ...`, m);
+  conn.sendFile(m.chat, url, filename, null, m);
+};
 
-} handler.command = /gitclone|clonarepo|clonarrepo|repoclonar/i
+handler.command = /gitclone|clonarepo|clonarrepo|repoclonar/i;
 
-export default handler
-handler.rowner= true
+export default handler;
+handler.rowner = true;
